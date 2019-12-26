da9dbf0757c30c1f669712976362b39b2b7cbdf1
Srikar Srivatsav <srikarsrivatsav@Srikars-MacBook-Air.local>
2019-12-26 23:00:41 +0530

    Header footer components

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,70 @@
 import React from "react";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
 
-export default function Footer() {
-  return <div>footer</div>;
+
+export default function Footer() 
+{
+  return <div>
+  <div>{/* Jumbotron element */}
+  <Jumbotron className='page-footer'>
+  <h1>Order Food Online</h1>
+  <p>
+    Millions of people around the world have already made Catersmart the place where their dream happens.
+  </p>
+  <p>
+    <Button variant="primary">Enquire Now</Button>
+  </p>
+  </Jumbotron></div>
+  
+  <div>
+<footer className="page-footer font-small pt-4 container">
+
+  <div className="container-fluid text-center text-md-left">
+
+    <div className="row">
+
+      <div className="ml-sm-4">
+        <p><a href="#!">About us</a></p>
+      </div>
+
+      <div className="ml-sm-4">
+      <p><a href="#!">Privacy and Terms</a></p>
+
+      </div>
+      <div className="ml-sm-4">
+      <p><a href="#!">Contact Us</a></p>
+
+      </div>
+      
+
+          <div className="fb-ic"><a href='#!' >
+            <i className="fab fa-facebook-f white-text mr-4"> </i>
+          </a></div>
+          <a href='#!' className="tw-ic">
+            <i className="fab fa-twitter white-text mr-4"> </i>
+          </a>
+          <a href='#!' className="gplus-ic">
+            <i className="fab fa-google-plus-g white-text mr-4"> </i>
+          </a>
+          <a href='#!' className="li-ic">
+            <i className="fab fa-linkedin-in white-text mr-4"> </i>
+          </a>
+          <a href='#!' className="ins-ic">
+            <i className="fab fa-instagram white-text"> </i>
+          </a>
+
+
+      
+
+    </div>
+
+  </div>
+
+</footer>
+  </div>
+  
+  </div>;
+
+
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import "../App.css";
 
-export default function Header() {
-  return <div>header</div>;
+
+
+export default function Header() 
+{
+  return <div>
+    <Navbar bg="white" expand="lg" className='container'>
+  <Navbar.Brand href="#home"><img className='caterLogo ' src={require('../catersmart.png')} ></img></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto" >
+      <Nav.Link href="#home">Your current location</Nav.Link>
+      <Nav.Link href="#home" className="mr-sm-2">Corporate solutions</Nav.Link>
+      <Nav.Link href="#home">Sign in</Nav.Link>
+
+
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+  </div>;
 }
